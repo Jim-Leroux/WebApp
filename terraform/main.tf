b5db61bf57746be26ff4f22b0b317a0825b706f0
Jim <jim@MacBook-Pro-de-Jade-6.local>
2025-03-04 15:27:07 +0100

    Push deployment part

---
+++ terraform/main.tf
@@ -0,0 +1,38 @@
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.azure_subscription_id
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+  tenant_id       = var.azure_tenant_id
+}
+
+
+resource "azurerm_resource_group" "rg_webapp" {
+  name     = "rg-webapp"
+  location = "France Central"
+}
+
+resource "azurerm_kubernetes_cluster" "aks_webapp" {
+  name                = "webapp"
+  location            = azurerm_resource_group.rg_webapp.location
+  resource_group_name = azurerm_resource_group.rg_webapp.name
+  dns_prefix          = "webapp"
+
+  default_node_pool {
+    name       = "nodepool1"
+    node_count = 1
+    vm_size    = "Standard_B1ms"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+output "aks_kube_config" {
+  description = "Kubeconfig file pour se connecter au cluster AKS"
+  value       = azurerm_kubernetes_cluster.aks_webapp.kube_config_raw
+  sensitive   = true 
+}
+
